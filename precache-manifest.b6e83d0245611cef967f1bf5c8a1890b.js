1a414c3c31095c20de6d3cfdfad9deda22bf3f6c
Clare Jolly <clarejollyuk@gmail.com>
2019-01-01 20:42:51 +0000

    Updates

---
+++ precache-manifest.b6e83d0245611cef967f1bf5c8a1890b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ca2bf6ccaf7c4a00d35",
+    "url": "/recipe-finder/static/css/main.84035528.chunk.css"
+  },
+  {
+    "revision": "1ca2bf6ccaf7c4a00d35",
+    "url": "/recipe-finder/static/js/main.1ca2bf6c.chunk.js"
+  },
+  {
+    "revision": "21493fe545096eea72f4",
+    "url": "/recipe-finder/static/js/1.21493fe5.chunk.js"
+  },
+  {
+    "revision": "693457eb8e1e72c33441",
+    "url": "/recipe-finder/static/js/runtime~main.693457eb.js"
+  },
+  {
+    "revision": "30340d4dcfec9d3aa490c8a5fa225e38",
+    "url": "/recipe-finder/index.html"
+  }
+];
